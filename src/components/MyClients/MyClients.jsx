65650c5590d0162cfc2a308c00509f8e2ed4f52d
rope25 <robertopelayo@gmail.com>
2025-09-16 22:41:53 +0200

    CRUD con boton

--- src/components/MyClients/MyClients.jsx
+++ src/components/MyClients/MyClients.jsx
@@ -1,3 +1,7 @@
+
+import styles from "./MyClients.module.css";
+
+
 // para registrar y autentificar usuarios y mejorar la seguridad del proyecto, limitando las acciones sobre la base de datos (requisito opcional)
 
 
@@ -5,8 +9,19 @@ export default function MyClients() {
 
 
   return (
-    <div>
-      <h1>Mis Clientes</h1>
-    </div>
+
+
+<div style={{
+  backgroundImage: "https://es.web.img3.acsta.net/r_1920_1080/pictures/14/11/07/17/57/195130.jpg')",
+  backgroundSize: "contain",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  
+}}>
+  {/* contenido del componente */}
+<h1>Mis Clientes</h1>
+
+</div>
+  
   ); 
 }
