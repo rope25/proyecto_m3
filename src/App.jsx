1b83520f9f4da8f82a36518e00f32931fff26a2e
rope25 <robertopelayo@gmail.com>
2025-09-25 20:41:49 +0200

    bootstrap30

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,23 @@ function App() {
   return (
 
  <main className="background">
-    
+    <header>
   <Listas />
   <MyClients />
- 
+</header>
+
+<img src="avatar" alt="ima" />
+  {/* <section class="row mb-4"> */}
+    <div>
+      <h2 class="text-primary">Bienvenidos</h2>
+      <p>Explora las funcionalidades básicas de Bootstrap 5 con este ejemplo sencillo.</p>
+    </div>
+  {/* </section> */}
+
 </main>
+
+
+
   )
 
 }
