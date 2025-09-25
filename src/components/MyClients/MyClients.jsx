3b7a1d6222375bee6f637277a38c5b405428d52e
rope25 <robertopelayo@gmail.com>
2025-09-25 19:34:51 +0200

    bootstrap10

--- src/components/MyClients/MyClients.jsx
+++ src/components/MyClients/MyClients.jsx
@@ -10,6 +10,39 @@ export default function MyClients() {
 
   return (
 
+     <section class="row mb-4">
+    <div class="col-12 text-center">
+      <h2 class="text-primary">Bienvenidos</h2>
+      <p>Explora las funcionalidades básicas de Bootstrap 5 con este ejemplo sencillo.</p>
+    </div>
+
+
+    <section class="row">
+    <div class="col-md-4 bg-light p-4">
+      <h3>Columna 1</h3>
+      <p>Bootstrap facilita el diseño responsivo. Esta columna ocupa un tercio del ancho en pantallas medianas o mayores.</p>
+    </div>
+    <div class="col-md-4 bg-secondary text-white p-4">
+      <h3>Columna 2</h3>
+      <p>Las clases predefinidas simplifican el estilo de los elementos.</p>
+    </div>
+    <div class="col-md-4 bg-light p-4">
+      <h3>Columna 3</h3>
+      <p>¡Crea páginas rápidamente con el sistema de cuadrícula de Bootstrap!</p>
+    </div>
+  
+
+
+
+ <section class="row mt-5">
+    <div class="col-12 text-center">
+      <img src="potenkim.jpg" alt="fondo" class="img-fluid"/>
+    </div>
+    <div class="col-md-12 d-flex justify-content-center ">
+      
+    </div>
+
+
 
 <div style={{
   backgroundImage: "https://es.web.img3.acsta.net/r_1920_1080/pictures/14/11/07/17/57/195130.jpg')",
@@ -18,10 +51,16 @@ export default function MyClients() {
   backgroundRepeat: "no-repeat",
   
 }}>
-  {/* contenido del componente */}
-<h1>Mis Clientes</h1>
-<img src="potenkim.jpg" alt="fondo" />
-</div>
   
+</div>
+</section>
+</section>
+</section>
+
+
+
+
+
+
   ); 
 }
